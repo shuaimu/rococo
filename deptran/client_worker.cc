6183927da0128f280a52428e52cfd771b4be22ef
lamont <lamont.nelson@gmail.com>
2015-12-23 03:25:22 -0500

    ...

--- deptran/client_worker.cc
+++ deptran/client_worker.cc
@@ -3,10 +3,6 @@
 
 namespace rococo {
 
-//ClientWorker::ClientWorker() {
-//  txn_req_factory_ = new TxnRequestFactory(Config::GetConfig()->sharding_);
-//}
-
 ClientWorker::~ClientWorker() {
   if (txn_req_factory_) {
     delete txn_req_factory_;
@@ -67,9 +63,6 @@ void ClientWorker::work() {
   delete timer_;
   return;
 }
-
-
-
 
 } // namespace rococo
 
